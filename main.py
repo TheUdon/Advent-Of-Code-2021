57b02d17e2d417ddebb1c6640442acf584fcb408
Don Kim <donkim9996@gmail.com>
2021-12-06 21:55:01 -0500

    Day 6 code
    First time ever using the collections module in Python

--- main.py
+++ main.py
@@ -1,113 +1,30 @@
+from collections import Counter
 # Setting up
-import json
-
-with open("day 5.txt") as file:
-    content = file.read()
-    data = content.split('\n')
-    data = [x.replace(" -> ", ",") for x in data]
-    data = [x.split(",") for x in data]
-    print(data)
-    coordinate_dict = {
-        "x1": [],
-        "y1": [],
-        "x2": [],
-        "y2": [],
-    }
-
-    for row in data:
-        coordinate_dict["x1"].append(int(row[0]))
-        coordinate_dict["y1"].append(int(row[1]))
-        coordinate_dict["x2"].append(int(row[2]))
-        coordinate_dict["y2"].append(int(row[3]))
-
-    print(json.dumps(coordinate_dict, indent=4))
-
-    max_x1 = max(coordinate_dict["x1"])
-    max_y1 = max(coordinate_dict["y1"])
-    # max_x2 = max(coordinate_dict["x2"])
-    # max_y2 = max(coordinate_dict["y2"])
-    # print(max_x1)
-    # print(max_y1)
-    # print(max_x2)
-    # print(max_y2)
-    # max x is 990 and max y is 989
-
-    grid = []
-    for y in range(max_y1):
-        grid.append([])
-        for x in range(max_x1):
-            grid[y].append(0)
-
+with open("day 6.txt") as file:
+    fish_times = list(map(int, next(file).split(",")))
     # Puzzle 1
-    # for num in range(len(data)):
-    #     x1 = coordinate_dict["x1"][num]
-    #     y1 = coordinate_dict["y1"][num]
-    #     x2 = coordinate_dict["x2"][num]
-    #     y2 = coordinate_dict["y2"][num]
-    #     if x1 == x2 or y1 == y2:
-    #         if x1 == x2:
-    #             if y1 > y2:
-    #                 for y_add in range(y2, y1+1):
-    #                     grid[y_add-1][x1-1] += 1
-    #             elif y2 > y1:
-    #                 for y_add in range(y1, y2+1):
-    #                     grid[y_add-1][x1-1] += 1
-    #         elif y1 == y2:
-    #             if x1 > x2:
-    #                 for x_add in range(x2, x1+1):
-    #                     grid[y1-1][x_add-1] += 1
-    #             elif x2 > x1:
-    #                 for x_add in range(x1, x2+1):
-    #                     grid[y1-1][x_add-1] += 1
+    # for day in range(80):
+    #     fish_times.extend([9] * fish_times.count(0))
+    #     fish_times = list(map(lambda fish: fish + 6 if fish == 0 else fish - 1, fish_times))
+    # print(len(fish_times))
 
     # Puzzle 2
-    for num in range(len(data)):
-        x1 = coordinate_dict["x1"][num]
-        y1 = coordinate_dict["y1"][num]
-        x2 = coordinate_dict["x2"][num]
-        y2 = coordinate_dict["y2"][num]
-        if x1 == x2 or y1 == y2:
-            if x1 == x2:
-                if y1 > y2:
-                    for y_add in range(y2, y1+1):
-                        grid[y_add-1][x1-1] += 1
-                elif y2 > y1:
-                    for y_add in range(y1, y2+1):
-                        grid[y_add-1][x1-1] += 1
-            elif y1 == y2:
-                if x1 > x2:
-                    for x_add in range(x2, x1+1):
-                        grid[y1-1][x_add-1] += 1
-                elif x2 > x1:
-                    for x_add in range(x1, x2+1):
-                        grid[y1-1][x_add-1] += 1
-        else:
-            if x1 > x2:
-                if y1 > y2:
-                    for y_add in range(y2, y1+1):
-                        grid[y1-1][x1-1] += 1
-                        y1 -= 1
-                        x1 -= 1
-                elif y2 > y1:
-                    for y_add in range(y1, y2+1):
-                        grid[y1-1][x1-1] += 1
-                        y1 += 1
-                        x1 -= 1
-            elif x2 > x1:
-                if y1 > y2:
-                    for y_add in range(y2, y1+1):
-                        grid[y1-1][x1-1] += 1
-                        y1 -= 1
-                        x1 += 1
-                elif y2 > y1:
-                    for y_add in range(y1, y2+1):
-                        grid[y1-1][x1-1] += 1
-                        y1 += 1
-                        x1 += 1
+    # First time using the collections module, had to look up a faster way to do a loop like this
+    fish_dict = Counter(fish_times)
+    print(fish_dict)
 
-    overlap = 0
-    for row in grid:
-        for number in row:
-            if number > 1:
-                overlap += 1
-    print(overlap)
+    for day in range(256):
+        zeroes = fish_dict[0]
+        fish_dict[0] = fish_dict[1]
+        fish_dict[1] = fish_dict[2]
+        fish_dict[2] = fish_dict[3]
+        fish_dict[3] = fish_dict[4]
+        fish_dict[4] = fish_dict[5]
+        fish_dict[5] = fish_dict[6]
+        fish_dict[6] = fish_dict[7]
+        fish_dict[6] += (zeroes)
+        fish_dict[7] = fish_dict[8]
+        fish_dict[8] = zeroes
+        print(day)
+
+    print(sum(x for x in fish_dict.values()))
