09f7b65f7558eaf07a7be0bef768e39e7d549ead
Don Kim <donkim9996@gmail.com>
2021-12-10 10:21:20 -0500

    Day 8 Code

--- main.py
+++ main.py
@@ -1,34 +1,110 @@
 # Setting up
-with open("day 7.txt") as file:
-    content = list(map(int, next(file).split(",")))
-    min_position = min(content)
-    max_position = max(content)
-    print(content)
-    print(min_position)
-    print(max_position)
-    # lowest horizontal position is 0 and the highest horizontal position is 1892
+with open("day 8.txt") as file:
+
+    digit_length_list = [2, 4, 3, 7]
+    # the lengths for 1, 4, 7. and 8 respectively
+
+    content = file.read().splitlines()
+    content = [i.split() for i in content]
+    data_list = []
+    for number in content:
+        data = []
+        for element in number:
+            if element == "|":
+                pass
+            else:
+                data.append(element)
+        data_list.append(data)
 
     # Puzzle 1
-    # fuel_list = []
-    # for x in range(min_position, max_position + 1):
-    #     fuel_use = []
-    #     for crab in content:
-    #         fuel = crab - x
-    #         fuel_use.append(abs(fuel))
-    #     total_fuel = sum(fuel_use)
-    #     fuel_list.append(total_fuel)
-    # print(fuel_list)
-    # print(min(fuel_list))
+    appearances = 0
+    for input in data_list:
+        for led in input[10:14]:
+            if len(led) in digit_length_list:
+                appearances += 1
+    print(appearances)
 
     # Puzzle 2
-    fuel_list = []
-    for x in range(min_position, max_position + 1):
-        fuel_use = []
-        for crab in content:
-            steps = abs(crab - x)
-            fuel = sum([step for step in range(1, steps + 1)])
-            fuel_use.append(fuel)
-        total_fuel = sum(fuel_use)
-        fuel_list.append(total_fuel)
-    print(fuel_list)
-    print(min(fuel_list))
+    outputs = []
+    for input in data_list:
+        number_dict = {}
+        for number in input[0:10]:
+            if len(number) == 2:
+                number_dict[1] = number
+            elif len(number) == 4:
+                number_dict[4] = number
+            elif len(number) == 3:
+                number_dict[7] = number
+            elif len(number) == 7:
+                number_dict[8] = number
+
+        for number in input[0:10]:
+            if len(number) == 6:
+                check = list(number_dict[4])
+                character_list = list(number)
+                for character in check:
+                    if character in character_list:
+                        character_list.remove(character)
+
+                if len(character_list) == 2:
+                    number_dict[9] = number
+
+                elif len(character_list) == 3:
+                    check = list(number_dict[1])
+                    character_list = list(number)
+                    for character in check:
+                        if character in character_list:
+                            character_list.remove(character)
+
+                    if len(character_list) == 5:
+                        number_dict[6] = number
+                    elif len(character_list) == 4:
+                        number_dict[0] = number
+
+            elif len(number) == 5:
+                check = list(number_dict[1])
+                character_list = list(number)
+                for character in check:
+                    if character in character_list:
+                        character_list.remove(character)
+
+                if len(character_list) == 3:
+                    number_dict[3] = number
+
+                elif len(character_list) == 4:
+                    check = list(number_dict[4])
+                    character_list = list(number)
+                    for character in check:
+                        if character in character_list:
+                            character_list.remove(character)
+                    if len(character_list) == 3:
+                        number_dict[2] = number
+                    elif len(character_list) == 2:
+                        number_dict[5] = number
+        print(input[10:14])
+        print(number_dict)
+        output = ""
+        for led in input[10:14]:
+            print(led)
+            if len(led) == 2:
+                output += str(1)
+            elif len(led) == 4:
+                output += str(4)
+            elif len(led) == 3:
+                output += str(7)
+            elif len(led) == 7:
+                output += str(8)
+            elif len(led) == 5 or len(led) == 6:
+                for key in number_dict:
+                    character_list = list(led)
+                    dictionary_list = list(number_dict[key])
+                    if len(led) == len(dictionary_list):
+                        for character in character_list:
+                            if character in dictionary_list:
+                                dictionary_list.remove(character)
+                        if len(dictionary_list) == 0:
+                            output += str(key)
+            print(output)
+        outputs.append(int(output))
+
+    print(sum(outputs))
