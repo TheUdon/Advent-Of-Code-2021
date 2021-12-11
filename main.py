645bd2faa1e475bae692ecb9b71758234dcf3fa3
Don Kim <donkim9996@gmail.com>
2021-12-11 11:28:52 -0500

    Day 9 code
    I had to look up how to solve Part 2 since I was not familiar with how to implement a Flood Fill

--- main.py
+++ main.py
@@ -1,110 +1,150 @@
-# Setting up
-with open("day 8.txt") as file:
-
-    digit_length_list = [2, 4, 3, 7]
-    # the lengths for 1, 4, 7. and 8 respectively
-
-    content = file.read().splitlines()
-    content = [i.split() for i in content]
-    data_list = []
-    for number in content:
-        data = []
-        for element in number:
-            if element == "|":
-                pass
-            else:
-                data.append(element)
-        data_list.append(data)
-
+with open("day 9.txt") as file:
     # Puzzle 1
-    appearances = 0
-    for input in data_list:
-        for led in input[10:14]:
-            if len(led) in digit_length_list:
-                appearances += 1
-    print(appearances)
+    # content = file.read().splitlines()
+    # content = [i.split() for i in content]
+    # low_points = []
+    # row_number = 0
+    # for row in content:
+    #     new_list = list(row[0])
+    #     index = 0
+    #     for number in new_list:
+    #         if row_number == 0 or row_number == 99:
+    #             if row_number == 0:
+    #                 up = 9
+    #                 below_row = list(content[row_number + 1])
+    #                 down = int(below_row[0][index])
+    #                 if index == 0:
+    #                     left = 9
+    #                     right = int(new_list[index + 1])
+    #                     if int(new_list[index]) < left and int(new_list[index]) < right and int(new_list[index]) < down and int(new_list[index]) < up:
+    #                         low_points.append(int(new_list[index]) + 1)
+    #                         index += 1
+    #                     else:
+    #                         index += 1
+    #                 elif index == 99:
+    #                     right = 9
+    #                     left = int(new_list[index - 1])
+    #                     if int(new_list[index]) < left and int(new_list[index]) < right and int(new_list[index]) < down and int(new_list[index]) < up:
+    #                         low_points.append(int(new_list[index]) + 1)
+    #                         index += 1
+    #                     else:
+    #                         index += 1
+    #                 else:
+    #                     left = int(new_list[index - 1])
+    #                     right = int(new_list[index + 1])
+    #                     if int(new_list[index]) < left and int(new_list[index]) < right and int(new_list[index]) < down and int(new_list[index]) < up:
+    #                         low_points.append(int(new_list[index]) + 1)
+    #                         index += 1
+    #                     else:
+    #                         index += 1
+    #
+    #             elif row_number == 99:
+    #                 down = 9
+    #                 above_row = list(content[row_number-1][0])
+    #                 up = int(above_row[index])
+    #                 if index == 0:
+    #                     left = 9
+    #                     right = int(new_list[index + 1])
+    #                     if int(new_list[index]) < left and int(new_list[index]) < right and int(
+    #                             new_list[index]) < down and int(new_list[index]) < up:
+    #                         low_points.append(int(new_list[index]) + 1)
+    #                         index += 1
+    #                     else:
+    #                         index += 1
+    #                 elif index == 99:
+    #                     right = 9
+    #                     left = int(new_list[index - 1])
+    #                     if int(new_list[index]) < left and int(new_list[index]) < right and int(
+    #                             new_list[index]) < down and int(new_list[index]) < up:
+    #                         low_points.append(int(new_list[index]) + 1)
+    #                         index += 1
+    #                     else:
+    #                         index += 1
+    #                 else:
+    #                     left = int(new_list[index - 1])
+    #                     right = int(new_list[index + 1])
+    #                     if int(new_list[index]) < left and int(new_list[index]) < right and int(
+    #                             new_list[index]) < down and int(new_list[index]) < up:
+    #                         low_points.append(int(new_list[index]) + 1)
+    #                         index += 1
+    #                     else:
+    #                         index += 1
+    #
+    #         else:
+    #             above_row = list(content[row_number - 1][0])
+    #             up = int(above_row[index])
+    #             below_row = list(content[row_number + 1][0])
+    #             down = int(below_row[index])
+    #             if index == 0:
+    #                 left = 9
+    #                 right = int(new_list[index + 1])
+    #                 if int(new_list[index]) < left and int(new_list[index]) < right and int(
+    #                         new_list[index]) < down and int(new_list[index]) < up:
+    #                     low_points.append(int(new_list[index]) + 1)
+    #                     index += 1
+    #                 else:
+    #                     index += 1
+    #             elif index == 99:
+    #                 right = 9
+    #                 left = int(new_list[index - 1])
+    #                 if int(new_list[index]) < left and int(new_list[index]) < right and int(
+    #                         new_list[index]) < down and int(new_list[index]) < up:
+    #                     low_points.append(int(new_list[index]) + 1)
+    #                     index += 1
+    #                 else:
+    #                     index += 1
+    #             else:
+    #                 left = int(new_list[index - 1])
+    #                 right = int(new_list[index + 1])
+    #                 if int(new_list[index]) < left and int(new_list[index]) < right and int(
+    #                         new_list[index]) < down and int(new_list[index]) < up:
+    #                     low_points.append(int(new_list[index]) + 1)
+    #                     index += 1
+    #                 else:
+    #                     index += 1
+    #     row_number += 1
+    # print(sum(low_points))
 
     # Puzzle 2
-    outputs = []
-    for input in data_list:
-        number_dict = {}
-        for number in input[0:10]:
-            if len(number) == 2:
-                number_dict[1] = number
-            elif len(number) == 4:
-                number_dict[4] = number
-            elif len(number) == 3:
-                number_dict[7] = number
-            elif len(number) == 7:
-                number_dict[8] = number
+    # Needed to find a different way to set up the data
+    # Learned that the concept for this puzzle is 'Flood Fill'(Like the bucket tool in MS Paint)
+    data = []
+    line = file.readline()
+    while line:
+        new_list = [x for x in line.strip('\n')]
+        int_line = [int(x) for x in new_list]
+        data.append(int_line)
+        line = file.readline()
+    print(data)
 
-        for number in input[0:10]:
-            if len(number) == 6:
-                check = list(number_dict[4])
-                character_list = list(number)
-                for character in check:
-                    if character in character_list:
-                        character_list.remove(character)
+    def get_value(data, position):
+        # position[0] should be the x coord and position[1] should be the y coord
+        if (position[0] < 0) or (position[0] >= len(data)):
+            return 10
+        if (position[1] < 0) or (position[1] >= len(data)):
+            return 10
+        return data[position[0]][position[1]]
 
-                if len(character_list) == 2:
-                    number_dict[9] = number
-
-                elif len(character_list) == 3:
-                    check = list(number_dict[1])
-                    character_list = list(number)
-                    for character in check:
-                        if character in character_list:
-                            character_list.remove(character)
-
-                    if len(character_list) == 5:
-                        number_dict[6] = number
-                    elif len(character_list) == 4:
-                        number_dict[0] = number
-
-            elif len(number) == 5:
-                check = list(number_dict[1])
-                character_list = list(number)
-                for character in check:
-                    if character in character_list:
-                        character_list.remove(character)
-
-                if len(character_list) == 3:
-                    number_dict[3] = number
-
-                elif len(character_list) == 4:
-                    check = list(number_dict[4])
-                    character_list = list(number)
-                    for character in check:
-                        if character in character_list:
-                            character_list.remove(character)
-                    if len(character_list) == 3:
-                        number_dict[2] = number
-                    elif len(character_list) == 2:
-                        number_dict[5] = number
-        print(input[10:14])
-        print(number_dict)
-        output = ""
-        for led in input[10:14]:
-            print(led)
-            if len(led) == 2:
-                output += str(1)
-            elif len(led) == 4:
-                output += str(4)
-            elif len(led) == 3:
-                output += str(7)
-            elif len(led) == 7:
-                output += str(8)
-            elif len(led) == 5 or len(led) == 6:
-                for key in number_dict:
-                    character_list = list(led)
-                    dictionary_list = list(number_dict[key])
-                    if len(led) == len(dictionary_list):
-                        for character in character_list:
-                            if character in dictionary_list:
-                                dictionary_list.remove(character)
-                        if len(dictionary_list) == 0:
-                            output += str(key)
-            print(output)
-        outputs.append(int(output))
-
-    print(sum(outputs))
+    basin_list = []
+    for x in range(len(data)):
+        for y in range(len(data[0])):
+            count = 0
+            open = [[x, y]]
+            closed = []
+            while len(open) > 0:
+                if (open[0] not in closed) and get_value(data, open[0]) < 9:
+                    count += 1
+                    closed.append(open[0])
+                    xx = open[0][0]
+                    yy = open[0][1]
+                    offsets = [[xx+1, yy], [xx-1, yy], [xx, yy+1], [xx, yy-1]]
+                    for element in offsets:
+                        open.append(element)
+                del(open[0])
+            for element in closed:
+                data[element[0]][element[1]] = 9
+            if count > 0:
+                basin_list.append(count)
+    basin_list.sort()
+    answer = basin_list[-1] * basin_list[-2] * basin_list[-3]
+    print(answer)
